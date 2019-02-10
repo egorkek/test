d049ad97e80270996517e5f4282c8352953062df
Егор <patron6813@gmail.com>
2019-02-10 15:03:33 +0300

    changes

---
+++ main.js
@@ -0,0 +1,17 @@
+function Ticker() {
+    this._i=0
+
+
+};
+Ticker.prototype={
+    tick:function () {
+        console.log(this._i++)
+
+    }
+}
+var ticker = new Ticker();
+
+setInterval(function () {
+    ticker.tick()
+
+}, 1000)
